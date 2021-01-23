980117326809e5a87dfa4e54c0187a4ede5c1b73
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2021-01-23 02:28:19 +0100

    Login form

---
+++ frontend/src/views/AdminLogin.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="login">
+      <v-card class="form">
+          <v-card-title>Login</v-card-title>
+          <v-card-text>
+            <v-text-field outlined dense label="Email"></v-text-field>
+            <v-text-field outlined dense label="Password" type="password"></v-text-field>
+          </v-card-text>
+          <v-divider class="pt-5"></v-divider>
+          <v-card-actions>
+              <v-btn block large color="primary" @click="login()" dark>Login</v-btn>
+          </v-card-actions>
+      </v-card>
+  </main>
+</template>
+
+<script>
+export default {
+    name: "AdminLogin",
+
+    methods: {
+        login(){
+            console.log('logging in')
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    main.login{
+        display: flex;
+        justify-content: center;
+    }
+
+    .form{
+        width: 80%;
+        margin-top: 25%;
+        height: fit-content;
+    }
+</style>
